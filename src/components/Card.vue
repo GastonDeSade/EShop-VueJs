f678190350089ae01bca2852c35c304232f42772
Gaston De Sade <gastondesade@proton.me>
2025-11-20 00:17:49 +0100

    Update the card component for add link-router

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,25 +1,27 @@
 <script setup lang="ts">
 import type { Product } from '@/types/product'
-
+defineOptions({ name: 'AppCard' })
 const props = defineProps<{ product?: Product }>()
 </script>
 
 <template>
   <div class="card card-sm bg-base-200 max-w-60 shadow">
-    <figure class="hover-gallery">
-      <img :src="props.product ? props.product.image : 'nothing'" />
-      <img :src="props.product ? props.product.image2 : 'nothing'" />
-      <img :src="props.product ? props.product.image3 : 'nothing'" />
-      <img :src="props.product ? props.product.image4 : 'nothing'" />
-    </figure>
-    <div class="card-body">
-      <h2 class="card-title flex justify-between">
-        {{ props.product ? props.product.name : '????' }}
-        <span class="font-normal">{{ props.product ? `$${props.product.price}` : '$??' }}</span>
-      </h2>
-      <p>
-        {{ props.product ? props.product.description : '????' }}
-      </p>
-    </div>
+    <router-link :to="`/product/${props.product ? props.product.id : ''}`">
+      <figure class="hover-gallery">
+        <img :src="props.product ? props.product.image : 'nothing'" />
+        <img :src="props.product ? props.product.image2 : 'nothing'" />
+        <img :src="props.product ? props.product.image3 : 'nothing'" />
+        <img :src="props.product ? props.product.image4 : 'nothing'" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title flex justify-between">
+          {{ props.product ? props.product.name : '????' }}
+          <span class="font-normal">{{ props.product ? `$${props.product.price}` : '$??' }}</span>
+        </h2>
+        <p>
+          {{ props.product ? props.product.description : '????' }}
+        </p>
+      </div>
+    </router-link>
   </div>
 </template>
