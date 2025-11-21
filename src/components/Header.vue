769c2c3786135cecd7f1b9e1055f9ccc0d443d1d
Gaston De Sade <gastondesade@proton.me>
2025-11-21 14:38:14 +0100

    Refactor Header component to streamline authentication buttons and integrate cart item count

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,10 +2,12 @@
 import { ref, watch, onMounted } from 'vue'
 import { useRouter, RouterLink } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
-import { SunMoon, Sun, ShoppingCart, ListOrdered } from 'lucide-vue-next'
+import { useCartStore } from '@/stores/cart'
+import { SunMoon, Sun, ShoppingCart, ListOrdered, LogOut, LogIn, UserPlus } from 'lucide-vue-next'
 
 const router = useRouter()
 const authStore = useAuthStore()
+const cartStore = useCartStore()
 
 const handleLogout = () => {
   authStore.logout()
@@ -33,60 +35,33 @@ watch(theme, (newTheme) => {
 <template>
   <div class="navbar h-full items-center bg-base-100 shadow-sm">
     <div class="navbar-start">
-      <div class="dropdown">
-        <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h7"
-            />
-          </svg>
-        </div>
-        <ul
-          tabindex="-1"
-          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-        >
-          <li><a>Homepage</a></li>
-          <li><a>Portfolio</a></li>
-          <li><a>About</a></li>
-          <li v-if="authStore.isAuthenticated">
-            <button @click="handleLogout" class="btn-secondary">Se déconnecter</button>
-          </li>
-          <div v-else>
-            <li><router-link to="/login" class="btn"> Se connecter </router-link></li>
-            <li><router-link to="/register" class="btn"> S'inscrire </router-link></li>
-          </div>
-        </ul>
+      <div v-if="authStore.isAuthenticated">
+        <button @click="handleLogout" class="btn btn-ghost btn-circle">
+          <div class="swap-off"><LogOut /></div>
+        </button>
+      </div>
+      <div v-else>
+        <RouterLink class="btn btn-ghost btn-circle" to="/login">
+          <div class="swap-off"><LogIn /></div>
+        </RouterLink>
+        <RouterLink class="btn btn-ghost btn-circle" to="/register">
+          <div class="swap-off"><UserPlus /></div>
+        </RouterLink>
       </div>
     </div>
     <div class="navbar-center">
       <router-link to="/" class="btn btn-ghost normal-case text-xl">Jade</router-link>
     </div>
     <div class="navbar-end">
-      <button class="btn btn-ghost btn-circle">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
-      </button>
+      <RouterLink class="btn btn-ghost btn-circle indicator" to="/cart">
+        <span class="indicator-item indicator-start badge bg-primary text-white">{{
+          cartStore.getTotalItems()
+        }}</span>
+        <div class="swap-off"><ShoppingCart /></div>
+      </RouterLink>
+      <RouterLink class="btn btn-ghost btn-circle" to="/order">
+        <div class="swap-off"><ListOrdered /></div>
+      </RouterLink>
       <button class="btn btn-ghost btn-circle">
         <label class="swap swap-rotate">
           <input type="checkbox" @click="toggleTheme" />
@@ -94,12 +69,6 @@ watch(theme, (newTheme) => {
           <div class="swap-off"><Sun /></div>
         </label>
       </button>
-      <RouterLink class="btn btn-ghost btn-circle" to="/cart">
-        <div class="swap-off"><ShoppingCart /></div>
-      </RouterLink>
-      <RouterLink class="btn btn-ghost btn-circle" to="/order">
-        <div class="swap-off"><ListOrdered /></div>
-      </RouterLink>
     </div>
   </div>
 </template>
